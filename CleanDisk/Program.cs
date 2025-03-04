a8690917a66cf3dd8f0336ca87237ea6f2f9b196
K0T3LL0 <akk.ignuta@yandex.ru>
2025-03-04 14:41:23 +0300

    Update and little optimize

--- CleanDisk/Program.cs
+++ CleanDisk/Program.cs
@@ -1,7 +1,5 @@
-﻿
-using System;
+﻿using System;
 using System.CodeDom;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
@@ -10,60 +8,35 @@ using System.Threading;
 using System.Threading.Tasks;
 
 
-namespace CleanDisk
+namespace CleanDiskRework
 {
     internal class Program
     {
+#pragma warning disable CA1416 // Проверка совместимости платформы
         static string user = System.Security.Principal.WindowsIdentity.GetCurrent().Name.Split('\\')[1];
+#pragma warning restore CA1416 // Проверка совместимости платформы
         static string virusDirectoryPath = $"C:\\Users\\{user}\\AppData\\Roaming\\WindowsServices";
         static string roamingPath = $"C:\\Users\\{user}\\AppData\\Roaming\\";
         static string path;
         static DriveInfo[] disks = DriveInfo.GetDrives();
 
-        static void ProcessToWork(string arguments)
-        {
-            Process process = new Process();
-            process.StartInfo = new ProcessStartInfo();
-            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.Arguments = arguments;
-            process.Start();
-        }
-
-
-        //C:\Users\Kotello\AppData\Roaming
-        static void Main(string[] args)
-        {
-            Unhide(user);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Всё проверено и сделано успешно!");
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("Big thanks K0T3LL0!");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Нажмите любую клавишу чтобы закрыть.");
-            Console.ReadKey();
-        }
-
-        static void Unhide(string user)
+        static async Task Unhide(string user)
         {
 
             if (!Directory.Exists(roamingPath))
                 Console.WriteLine(false);
-            else
-                ProcessToWork($"/c cd {roamingPath} & attrib -r -a -s -h /d /s");
-            DeletingVirus(user);
-            DeletingVirusFromFlash();
-            
+            await DeletingVirus(user);
+            await DeletingVirusFromFlash();
         }
 
+        static Task DeletingVirus(string user = "")
+        {
 
-        static async Task DeletingVirus(string user = "")
-        { 
             Process[] procs = Process.GetProcessesByName("wscript");
             foreach (Process proc in procs)
                 if (proc.ProcessName == "wscript")
                 {
-                    Console.WriteLine("Убил");
+                    Console.WriteLine("Убил процесс вируса");
                     proc.Kill();
                 }
             TryAgain:
@@ -82,68 +55,50 @@ namespace CleanDisk
             }
             else
                 Console.WriteLine("На компьютере нету вируса");
-            await Task.CompletedTask;
+            return Task.CompletedTask;
         }
 
-        static void DeletingVirusFromFlash()
+        static async Task DeletingVirusFromFlash()
         {
-            List<string> disk_letter = disks
-                .Select(x => x.IsReady && x.DriveType == DriveType.Removable ? x.Name.Substring(0, 2) : null)
-                .SkipWhile(x => x == null)
-                .ToList();
+            var disk_letter = disks.Select(x => x.IsReady && x.DriveType == DriveType.Removable ? x.Name.Substring(0, 2) : null).ToList();
             disk_letter.RemoveAll(x => x == null);
+            Console.WriteLine(string.Join(", ", disk_letter));
             foreach (var flash in disk_letter)
             {
-            Link:
-                //Console.WriteLine("Укажите путь к флешке(пример E)");
-                
-                path = flash;//H + ":";
+                path = flash;
                 string virusFlashPath = $"{path}WindowsServices";
             Link2:
                 if (Directory.Exists(path))
+                    GetFilesBack(path);
+                if (Directory.Exists($"{virusFlashPath}"))
                 {
-                    Process.Start("cmd.exe", $"/c {path} && attrib -r -a -s -h /d /s");
-
-                    if (Directory.Exists($"{virusFlashPath}"))
+                    try
                     {
-                        try
-                        {
-                            foreach (string files in Directory.GetFiles(virusFlashPath))
-                                File.Delete(files);
-                            Directory.Delete(virusFlashPath);
-                        }
-                        catch (Exception)
-                        {
-                            goto Link2;
-                        }
+                        foreach (string files in Directory.GetFiles(virusFlashPath))
+                            File.Delete(files);
+                        Directory.Delete(virusFlashPath);
                     }
-                    else
+                    catch (Exception)
                     {
-                        Console.WriteLine("Папка с вирусом не найдена!");
+                        goto Link2;
                     }
-
                 }
                 else
                 {
-                    Console.WriteLine("Диск не найден. Попробуйте ещё раз");
-                    goto Link;
+                    Console.WriteLine("Папка с вирусом не найдена!");
                 }
-                GetFilesBack(path);
-
             }
+            await Task.CompletedTask;
+
         }
-       
+
         static void GetFilesBack(string pathToFileMove)
         {
             //string name = disks.First(x => x.IsReady && x.DriveType == DriveType.Removable).VolumeLabel; // в метод First && x.Name == $"{path}\\"
             string name = disks.Select(x => path == x.Name.Substring(0, 2) ? x.VolumeLabel : null).SkipWhile(x => x == null).ToArray()[0];
-            Console.WriteLine(path+"\\" + $"{name}.lnk");
+            Console.WriteLine(path + "\\" + $"{name}.lnk");
             if (File.Exists($"{path}\\{name}.lnk"))
                 File.Delete(pathToFileMove + name + ".lnk");//
-            else
-            {
-                Console.WriteLine("sosi");
-            }
             try
             {
                 if (Directory.Exists($"{pathToFileMove}_"))
@@ -153,9 +108,19 @@ namespace CleanDisk
             {
                 Console.WriteLine(ex);
             }
-           
-           
         }
-       
+
+        static async Task Main(string[] args)
+        {
+            await Unhide(user);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Всё проверено и сделано успешно!");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Big thanks K0T3LL0!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Нажмите любую клавишу чтобы закрыть.");
+            Console.ReadKey();
+        }
+
     }
 }
